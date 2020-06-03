141c318852edbc1cf0daab74014a16e9fcb5b048
kugi <kugimania@gmail.com>
2020-06-04 02:20:12 +0900

    Implement Vec3

--- Google_tests/Vec3Test.cpp
+++ Google_tests/Vec3Test.cpp
@@ -1,33 +1,121 @@
 #include "gtest/gtest.h"
 #include "../vec3.hpp"
 
-TEST(Vec3Init,  CorrectXVal){
+TEST(Vec3Init, CorrectVal){
     Vec3 v(1.0, 2.0, 3.0);
     EXPECT_DOUBLE_EQ(v.x, 1.0);
-}
-
-TEST(Vec3Init,  CorrectYVal){
-    Vec3 v(1.0, 2.0, 3.0);
     EXPECT_DOUBLE_EQ(v.y, 2.0);
-}
-
-TEST(Vec3Init,  CorrectZVal){
-    Vec3 v(1.0, 2.0, 3.0);
     EXPECT_DOUBLE_EQ(v.z, 3.0);
 }
 
 //Default Constructor
-TEST(Vec3Init,  CorrectDefaultXVal){
+TEST(Vec3Init, CorrectDefaultVal){
     Vec3 v;
     EXPECT_DOUBLE_EQ(v.x, 1.0);
-}
-
-TEST(Vec3Init,  CorrectDefaultYVal){
-    Vec3 v;
     EXPECT_DOUBLE_EQ(v.y, 1.0);
+    EXPECT_DOUBLE_EQ(v.z, 1.0);
 }
 
-TEST(Vec3Init,  CorrectDefaultZVal){
-    Vec3 v;
-    EXPECT_DOUBLE_EQ(v.z, 1.0);
+//PosNeg
+TEST(Vec3PosNeg, Pos){
+    Vec3 v1;
+    Vec3 v2(1.0, 1.0, 1.0);
+    v1 = +v1;
+    EXPECT_DOUBLE_EQ(v1.x, v2.x);
+    EXPECT_DOUBLE_EQ(v1.y, v2.y);
+    EXPECT_DOUBLE_EQ(v1.z, v2.z);
+}
+
+TEST(Vec3PosNeg, Neg){
+    Vec3 v1;
+    Vec3 v2(-1.0, -1.0, -1.0);
+    v1 = -v1;
+    EXPECT_DOUBLE_EQ(v1.x, v2.x);
+    EXPECT_DOUBLE_EQ(v1.y, v2.y);
+    EXPECT_DOUBLE_EQ(v1.z, v2.z);
+}
+
+TEST(Arithmetic, Add){
+    Vec3 v1;
+    Vec3 v2(1.0, 2.0, 3.0);
+    Vec3 value = v1 + v2;
+    EXPECT_DOUBLE_EQ(value.x, 2.0);
+    EXPECT_DOUBLE_EQ(value.y, 3.0);
+    EXPECT_DOUBLE_EQ(value.z, 4.0);
+}
+
+TEST(Arithmetic, AddEq){
+    Vec3 value;
+    Vec3 operand(1.0, 2.0, 3.0);
+    value += operand;
+    EXPECT_DOUBLE_EQ(value.x, 2.0);
+    EXPECT_DOUBLE_EQ(value.y, 3.0);
+    EXPECT_DOUBLE_EQ(value.z, 4.0);
+}
+
+TEST(Arithmetic, Sub){
+    Vec3 v1;
+    Vec3 v2(1.0, 2.0, 3.0);
+    Vec3 value = v2 - v1;
+    EXPECT_DOUBLE_EQ(value.x, 0.0);
+    EXPECT_DOUBLE_EQ(value.y, 1.0);
+    EXPECT_DOUBLE_EQ(value.z, 2.0);
+}
+
+TEST(Arithmetic, SubEq){
+    Vec3 value;
+    Vec3 operand(1.0, 2.0, 3.0);
+    value -= operand;
+    EXPECT_DOUBLE_EQ(value.x, 0.0);
+    EXPECT_DOUBLE_EQ(value.y, -1.0);
+    EXPECT_DOUBLE_EQ(value.z, -2.0);
+}
+
+TEST(Arithmetic, Mul){
+    Vec3 v1;
+    Vec3 v2(1.0, 2.0, 3.0);
+    Vec3 value = v1 * v2;
+    EXPECT_DOUBLE_EQ(value.x, 1.0);
+    EXPECT_DOUBLE_EQ(value.y, 2.0);
+    EXPECT_DOUBLE_EQ(value.z, 3.0);
+}
+
+TEST(Arithmetic, MulEq){
+    Vec3 value;
+    Vec3 operand(1.0, 2.0, 3.0);
+    value *= operand;
+    EXPECT_DOUBLE_EQ(value.x, 1.0);
+    EXPECT_DOUBLE_EQ(value.y, 2.0);
+    EXPECT_DOUBLE_EQ(value.z, 3.0);
+}
+
+TEST(Arithmetic, Div){
+    Vec3 v1;
+    Vec3 v2(1.0, 2.0, 4.0);
+    Vec3 value = v1 / v2;
+    EXPECT_DOUBLE_EQ(value.x, 1.0);
+    EXPECT_DOUBLE_EQ(value.y, 0.5);
+    EXPECT_DOUBLE_EQ(value.z, 0.25);
+}
+
+TEST(Arithmetic, DivEq){
+    Vec3 value;
+    Vec3 operand(1.0, 2.0, 4.0);
+    value /= operand;
+    EXPECT_DOUBLE_EQ(value.x, 1.0);
+    EXPECT_DOUBLE_EQ(value.y, 0.5);
+    EXPECT_DOUBLE_EQ(value.z, 0.25);
+}
+
+TEST(Length, CorrectVal){
+    Vec3 v(3.0, 4.0, 0.0);
+    EXPECT_DOUBLE_EQ(v.length(), 5.0);
+}
+
+TEST(Unit, CorrectVal){
+    Vec3 v(0.0, 2.0, 0.0);
+    Vec3 unit = v.unit();
+    EXPECT_DOUBLE_EQ(unit.x, 0.0);
+    EXPECT_DOUBLE_EQ(unit.y, 1.0);
+    EXPECT_DOUBLE_EQ(unit.z, 0.0);
 }
--- vec3.hpp
+++ vec3.hpp
@@ -1,10 +1,110 @@
 #ifndef GENJI_VEC3_HPP
 #define GENJI_VEC3_HPP
 
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
 struct Vec3 {
     double x, y, z;
     Vec3() : x(1.0), y(1.0), z(1.0) {}
     Vec3(double _x, double _y, double _z) : x(_x), y(_y), z(_z) {}
+    const Vec3& operator+() const { return *this; }
+    const Vec3& operator-() const { return Vec3(-x, -y, -z); }
+    inline Vec3& operator+=(const Vec3& operand);
+    inline Vec3& operator-=(const Vec3& operand);
+    inline Vec3& operator*=(const Vec3& operand);
+    inline Vec3& operator/=(const Vec3& operand);
+    inline Vec3& operator*=(const double scale);
+    inline Vec3& operator/=(const double scale);
+    inline double length();
+    inline Vec3& unit();
 };
 
+inline Vec3 operator+(const Vec3& v1, const Vec3& v2) {
+    return Vec3(v1.x + v2.x, v1.y + v2.y, v1.z + v2.z);
+}
+
+inline Vec3 operator-(const Vec3& v1, const Vec3& v2) {
+    return Vec3(v1.x - v2.x, v1.y - v2.y, v1.z - v2.z);
+}
+
+inline Vec3 operator*(const Vec3& v1, const Vec3& v2) {
+    return Vec3(v1.x * v2.x, v1.y * v2.y, v1.z * v2.z);
+}
+
+inline Vec3 operator/(const Vec3& v1, const Vec3& v2) {
+    try {
+        v1.x / v2.x;
+        v1.y / v2.y;
+        v1.z / v2.z;
+    }
+    catch (std::logic_error e) {
+        std::cerr << e.what();
+    }
+    return Vec3(v1.x / v2.x, v1.y / v2.y, v1.z / v2.z);
+}
+
+inline Vec3& Vec3::operator+=(const Vec3& operand){
+    x += operand.x;
+    y += operand.y;
+    z += operand.z;
+    return *this;
+}
+
+inline Vec3& Vec3::operator-=(const Vec3& operand){
+    x -= operand.x;
+    y -= operand.y;
+    z -= operand.z;
+    return *this;
+}
+
+inline Vec3& Vec3::operator*=(const Vec3& operand) {
+    x *= operand.x;
+    y *= operand.y;
+    z *= operand.z;
+    return *this;
+}
+
+inline Vec3& Vec3::operator/=(const Vec3& operand) {
+    try {
+        x /= operand.x;
+        y /= operand.y;
+        z /= operand.z;
+    }
+    catch (logic_error e) {
+        cerr << e.what();
+    }
+    return *this;
+}
+
+inline Vec3& Vec3::operator*=(const double scale) {
+    x *= scale;
+    y *= scale;
+    z *= scale;
+}
+
+inline Vec3& Vec3::operator/=(const double scale) {
+    try {
+        x /= scale;
+        y /= scale;
+        z /= scale;
+    }
+    catch (logic_error e) {
+        cerr << e.what();
+    }
+    return *this;
+}
+
+inline double Vec3::length() {
+    return sqrt(x*x + y*y + z*z);
+}
+
+inline Vec3& Vec3::unit() {
+    x /= this->length();
+    y /= this->length();
+    z /= this->length();
+    return *this;
+}
 #endif //GENJI_VEC3_HPP
