99313308316d964bda21f28f029ae52e9ff04768
kugi <kugimania@gmail.com>
2020-06-06 23:41:47 +0900

    Change project to myVec3

--- README.md
+++ README.md
@@ -1,10 +1,16 @@
-# Genji
+# myVec3
 
-A simple ray tracer project.
+Implementation of _Vec3 structure_ in C++ using *Google Test*
 
-## Things to be implemented
-- A Simple Ray Tracer
-- Distributed Ray Tracer
-- Photon Mapper
-- Progressive Photon Mapper
-- Path Tracer
+[Google Test setup for CLion](https://www.jetbrains.com/help/clion/creating-google-test-run-debug-configuration-for-test.html)
+
+Result of [main.cpp](main.cpp)
+
+```
+v1 = (1,2,3)
+v2 = (3,2,1)
+v1 + v2 = (4,4,4)
+v1 - v2 = (-2,0,2)
+v1 * v2 = (3,4,3)
+v1 / v2 = (0.333333,1,3)
+```
--- main.cpp
+++ main.cpp
@@ -1,6 +1,28 @@
-#include <iostream>
+#include "vec3.hpp"
+
+void printVec3(Vec3 v);
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    Vec3 v1(1.0, 2.0, 3.0);
+    Vec3 v2(3.0, 2.0, 1.0);
+    cout << "v1 = ";
+    printVec3(v1);
+    cout << "v2 = ";
+    printVec3(v2);
+    cout << "v1 + v2 = ";
+    printVec3(v1 + v2);
+    cout << "v1 - v2 = ";
+    printVec3(v1 - v2);
+    cout << "v1 * v2 = ";
+    printVec3(v1 * v2);
+    cout << "v1 / v2 = ";
+    printVec3(v1 / v2);
     return 0;
+}
+
+void printVec3(Vec3 v) {
+    cout << "(" << v.x
+         << "," << v.y
+         << "," << v.z
+         << ")" << endl;
 }
